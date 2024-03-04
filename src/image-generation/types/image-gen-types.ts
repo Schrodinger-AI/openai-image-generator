7078a95b554195f82ed8a759314d2aba98bcc40c
kanthgithub <kanth.explorer@gmail.com>
2024-03-04 19:04:28 +0800

    doc: swapper APiProperty documentation added to request and response schemas

--- src/image-generation/types/image-gen-types.ts
+++ src/image-generation/types/image-gen-types.ts
@@ -1,53 +1,68 @@
-import { IsString, IsArray, IsObject, ValidateNested, IsNotEmpty, IsBase64, validate, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class Trait {
+    @ApiProperty({ description: 'The name of the trait.' })
     name: string;
+
+    @ApiProperty({ description: 'The value of the trait.' })
     value: string;
 }
 
+export class ImageDescription {
+    @ApiProperty({ description: 'The image data.' })
+    image: string;
+
+    @ApiProperty({ description: 'The traits of the image.', type: [Trait] })
+    traits: Trait[];
+
+    @ApiProperty({ description: 'The extra data for the image.' })
+    extraData: string;
+}
+
 export class ImageGenerationRequest {
+    @ApiProperty({ description: 'The seed for image generation.' })
     seed: string;
+
+    @ApiProperty({ description: 'The new traits for the image.', type: [Trait] })
     newTraits: Trait[];
+
+    @ApiProperty({ description: 'The base image description.', type: ImageDescription })
     baseImage: ImageDescription;
 }
 
 export class ImageGenerationResponseOk {
+    @ApiProperty({ description: 'The request ID.' })
     requestId: string;
 }
 
 export class ImageGenerationResponseNotOk {
+    @ApiProperty({ description: 'The error message.' })
     error: string;
 }
 
 export class ImageQueryRequest {
+    @ApiProperty({ description: 'The request ID.' })
     requestId: string;
 }
 
 export class ImageQueryResponseOk {
+    @ApiProperty({ description: 'The list of image descriptions.', type: [ImageDescription] })
     images: ImageDescription[];
 }
 
 export class ImageQueryResponseNotOk {
+    @ApiProperty({ description: 'The error message.' })
     error: string;
 }
 
-export class ImageDescription {
-    image: string;
-    traits: Trait[];
-    extraData: string;
-}
-
 export class TraitValue {
-    @IsString()
-    @IsNotEmpty()
+    @ApiProperty({ description: 'The name of the trait.' })
     name: string;
 
-    @IsArray()
-    @IsString({ each: true })
+    @ApiProperty({ description: 'The possible values for the trait.' })
     values: string[];
 
-    @IsString()
-    @IsNotEmpty()
+    @ApiProperty({ description: 'The variation of the trait.' })
     variation: string;
 }
 
